be2ad780bd6049e4309ebc14802647230b0835ae
Preslav977 <p.cvetanow@gmail.com>
2026-02-07 10:19:45 +0200

    Added instructions to show more jobs on the page

--- interfaces/JobsInterface/JobsInterface.d.ts
+++ interfaces/JobsInterface/JobsInterface.d.ts
@@ -1,12 +1,13 @@
 export interface JobsInterface {
     id: number;
     hybridOrRemote: string;
-    joqQualification: string;
+    joqQualification?: string;
     fullTimeOrNot: string;
     location: string;
     datePosted: Date;
     jobTitle: string;
     jobDescription: string;
+    jobAnchor?: string;
     companyID: number;
 }
 //# sourceMappingURL=JobsInterface.d.ts.map
