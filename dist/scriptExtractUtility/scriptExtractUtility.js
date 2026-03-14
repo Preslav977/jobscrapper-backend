805f682f42f2e6d11961c2a0c71b5078373a4342
Preslav977 <p.cvetanow@gmail.com>
2026-03-14 11:18:06 +0200

    Created utility function to extrat the text, attribute from the fields gave it object type in elementField param

--- dist/scriptExtractUtility/scriptExtractUtility.js
+++ dist/scriptExtractUtility/scriptExtractUtility.js
@@ -1,3 +1,24 @@
-async function extractJobsText(page, { container, title, location, remoteOrHybrid, datePosted, anchorHref, }) { }
+async function extractJobsText(page, { container, title, location, remoteOrHybrid, datePosted, anchorHref, }) {
+    const doesJobContainerExists = (await page.waitForSelector(container.selector));
+    if (doesJobContainerExists) {
+        const result = await page.evaluate((container, title, location, remoteOrHybrid, datePosted, anchorHref) => {
+            function extractField(HTMLElement, elementField) {
+                if (HTMLElement === null || elementField === null)
+                    return null;
+                if (elementField.extractType === "text") {
+                    return HTMLElement.querySelector(elementField.selector)
+                        ?.textContent.trim()
+                        .replace("\n", "");
+                }
+                if (elementField.extractType === "attribute") {
+                    return HTMLElement.getAttribute(elementField.attr);
+                }
+                if (elementField.extractType === "parentElementAttribute") {
+                    return HTMLElement.querySelector(elementField.selector)?.getAttribute(elementField.attr);
+                }
+            }
+        }, container, title, location, remoteOrHybrid, datePosted, anchorHref);
+    }
+}
 export {};
 //# sourceMappingURL=scriptExtractUtility.js.map
--- scriptExtractUtility/scriptExtractUtility.ts
+++ scriptExtractUtility/scriptExtractUtility.ts
@@ -1,4 +1,4 @@
-import type { Page } from "puppeteer";
+import type { ElementHandle, Page } from "puppeteer";
 import type { ExtractionConfig } from "../interfaces/InstructionsInterface/InstructionsInterface.js";
 
 async function extractJobsText(
@@ -11,4 +11,43 @@ async function extractJobsText(
     datePosted,
     anchorHref,
   }: ExtractionConfig,
-) {}
+) {
+  const doesJobContainerExists = (await page.waitForSelector(
+    container.selector,
+  )) as ElementHandle<HTMLElement>;
+
+  if (doesJobContainerExists) {
+    const result = await page.evaluate(
+      (container, title, location, remoteOrHybrid, datePosted, anchorHref) => {
+        function extractField(
+          HTMLElement: HTMLElement,
+          elementField: { extractType: string; selector: string; attr: string },
+        ) {
+          if (HTMLElement === null || elementField === null) return null;
+
+          if (elementField.extractType === "text") {
+            return HTMLElement.querySelector(elementField.selector)
+              ?.textContent.trim()
+              .replace("\n", "");
+          }
+
+          if (elementField.extractType === "attribute") {
+            return HTMLElement.getAttribute(elementField.attr);
+          }
+
+          if (elementField.extractType === "parentElementAttribute") {
+            return HTMLElement.querySelector(
+              elementField.selector,
+            )?.getAttribute(elementField.attr);
+          }
+        }
+      },
+      container,
+      title,
+      location,
+      remoteOrHybrid,
+      datePosted,
+      anchorHref,
+    );
+  }
+}
