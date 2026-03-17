948efa26e20fab1fe005abea2982b6a5ab2e304c
Preslav977 <p.cvetanow@gmail.com>
2026-03-17 10:25:14 +0200

    Created helper utilities to randomize resolution, timezone and called the functions inside the switch block

--- scriptScrapingJobsSites/scriptScrapingJobsSites.ts
+++ scriptScrapingJobsSites/scriptScrapingJobsSites.ts
@@ -1,12 +1,23 @@
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import type { CompanyInterface } from "../interfaces/CompanyInterface/CompanyInterface.js";
-import { tryClick } from "../scriptNavigationUtility/scriptNavigationUtility.js";
+import {
+  getRandomTimezone,
+  height,
+  width,
+} from "../scriptHelperUtilities/scriptHelperUtilities.js";
+import {
+  selectOptionFromDropDown,
+  sleepDelay,
+  tryClick,
+  tryClickEvaluate,
+  tryClickLoadMore,
+} from "../scriptNavigationUtility/scriptNavigationUtility.js";
 
 puppeteer.default.use(StealthPlugin());
 
 export async function scriptScrapJobsSites(companySite: CompanyInterface) {
-  const { URL, browserNavigation, instructions, steps } = companySite;
+  const { URL, name, instructions, steps } = companySite;
 
   const browser = await puppeteer.default.launch({
     headless: false,
@@ -19,24 +30,80 @@ export async function scriptScrapJobsSites(companySite: CompanyInterface) {
     waitUntil: "load",
   });
 
-  await page!.setViewport({ width: 1920, height: 1080 });
+  await page!.setViewport({ width: width, height: height });
 
-  await page!.emulateTimezone("Europe/Sofia");
+  await page!.emulateTimezone(`${getRandomTimezone}`);
+
+  await sleepDelay(2500);
 
   await page!.evaluate(() => {
     window.scrollTo(0, document.body.scrollHeight);
   });
 
+  const navigationResults = [];
+
   try {
     for (const step of steps) {
       switch (step.action) {
-        case "click":
-          await tryClick(page, step.selector, 3);
+        case "click": {
+          const tryClickResult = await tryClick(page, step.selector, 3);
+
+          navigationResults.push({
+            step: step.selector,
+            status: tryClickResult,
+          });
 
           break;
+        }
 
-        default:
+        case "clickEvaluate": {
+          const tryClickEvaluateResult = await tryClickEvaluate(
+            page,
+            step.selector,
+            3,
+          );
+
+          navigationResults.push({
+            step: step.selector,
+            status: tryClickEvaluateResult,
+          });
+
           break;
+        }
+
+        case "clickMore": {
+          const tryClickMoreResult = await tryClickLoadMore(
+            page,
+            step.selector,
+          );
+
+          navigationResults.push({
+            step: step.selector,
+            status: tryClickMoreResult,
+          });
+
+          break;
+        }
+
+        case "select": {
+          const selectOptionFromDropDownResult = await selectOptionFromDropDown(
+            page,
+            step.selector,
+            step.option,
+            3,
+          );
+
+          navigationResults.push({
+            step: step.selector,
+            status: selectOptionFromDropDownResult,
+          });
+
+          break;
+        }
+
+        default: {
+          break;
+        }
       }
     }
   } catch (error) {
