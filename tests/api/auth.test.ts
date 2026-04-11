929c4b36dbaf0f0e079109908b2338356ca90151
Preslav977 <p.cvetanow@gmail.com>
2026-04-11 10:50:11 +0300

    Test if email is not 6 character long. Not sure if I should give the body a type or not

--- tests/api/auth.test.ts
+++ tests/api/auth.test.ts
@@ -6,6 +6,8 @@ import { authRouter } from "../../routes/authRouter/authRouter.js";
 
 import request from "supertest";
 
+import type { Response } from "supertest";
+
 // import jwt from 'jsonwebtoken'
 
 import { beforeEach, describe, expect, it } from "vitest";
@@ -20,19 +22,21 @@ describe("testing auth controller and routes", () => {
   });
 
   describe("[POST] /signup", () => {
-    it("use should able to signup", async () => {
-      const { body, header, status } = await request(app).post("/signup").send({
-        firstName: "",
-        lastName: "",
-        password: "12345678BG",
-        confirmPassword: "12345678BG",
-        location: "",
-        email: "test@abv.bg",
-        phoneNumber: "",
-        linkedInURL: "",
-        githubURL: "",
-        portfolioURL: "",
-      });
+    it("user should able to signup", async () => {
+      const { body, header, status }: Response = await request(app)
+        .post("/signup")
+        .send({
+          firstName: "",
+          lastName: "",
+          password: "12345678BG",
+          confirmPassword: "12345678BG",
+          location: "",
+          email: "test@abv.bg",
+          phoneNumber: "",
+          linkedInURL: "",
+          githubURL: "",
+          portfolioURL: "",
+        });
 
       const findTheSignedUpUser = await prisma.user.findFirst();
 
@@ -45,6 +49,31 @@ describe("testing auth controller and routes", () => {
       expect(body.email).toMatch("test@abv.bg");
 
       expect(body.password).toMatch(body.confirmPassword);
+    });
+
+    it("user shouldn't able to signup if email not 6 characters", async () => {
+      const { body, header, status } = await request(app).post("/signup").send({
+        firstName: "",
+        lastName: "",
+        password: "12345678BG",
+        confirmPassword: "12345678BG",
+        location: "",
+        email: "e@t.c",
+        phoneNumber: "",
+        linkedInURL: "",
+        githubURL: "",
+        portfolioURL: "",
+      });
+
+      const findTheSignedUpUser = await prisma.user.findFirst();
+
+      expect(findTheSignedUpUser).toBeNull();
+
+      expect(header["content-type"]).toMatch(/json/);
+
+      expect(status).toBe(400);
+
+      expect(body[0].msg).toEqual("Email must be at least 6 characters!");
     });
   });
 });
