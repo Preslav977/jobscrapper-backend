fe8058d1194b8e93798178aaab979fb457295edb
Preslav977 <p.cvetanow@gmail.com>
2026-04-16 11:22:29 +0300

    Test for tryClick to fail it element has not been found and attempts are lower than 1

--- tests/integration/tryClick.test.ts
+++ tests/integration/tryClick.test.ts
@@ -16,8 +16,42 @@ describe("tryClick integration", () => {
       "http://127.0.0.1:5500/tests/test-fixtures/element.click.html",
     );
 
-    const result = await tryClick(mockPage, ".btn", 5);
+    const result = await tryClick(mockPage, ".btn", 1);
 
     expect(result).toBe("success");
   });
+
+  it("tryClick should fail if the element doesn't exist on page", async () => {
+    const browser = await puppeteer.default.launch({
+      headless: true,
+      args: ["--no-sandbox"],
+    });
+
+    const mockPage: Page = await browser.newPage();
+
+    await mockPage.goto(
+      "http://127.0.0.1:5500/tests/test-fixtures/element.click.html",
+    );
+
+    const result = await tryClick(mockPage, "btn", 1);
+
+    expect(result).toBe("failure");
+  }, 20000);
+
+  it("tryClick should fail if attempts are lower than 1", async () => {
+    const browser = await puppeteer.default.launch({
+      headless: true,
+      args: ["--no-sandbox"],
+    });
+
+    const mockPage: Page = await browser.newPage();
+
+    await mockPage.goto(
+      "http://127.0.0.1:5500/tests/test-fixtures/element.click.html",
+    );
+
+    const result = await tryClick(mockPage, "btn", 0);
+
+    expect(result).toBe("failure");
+  }, 10000);
 });
